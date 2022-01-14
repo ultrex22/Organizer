a1d37f0b3eddd7cdbb651516906b1f471c740a05
Ronald <rontbrilhante@Gmail.com>
2022-01-13 21:19:20 -0500

    templates worked on more, added pic, colors.

--- organizer/settings.py
+++ organizer/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    'theme'
+    'theme',
+    'organizer',
+    'notes',
 ]
 
 TAILWIND_APP_NAME = 'theme'
