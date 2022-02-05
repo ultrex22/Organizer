310f2db47df2a3507bb1ede7511075ec011df5c0
Ronald <rontbrilhante@Gmail.com>
2022-02-05 18:01:25 -0500

    added borders, background image etc.

--- organizer/settings.py
+++ organizer/settings.py
@@ -141,9 +141,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR + "/static/organizer",
+    
+]
+
 
 MEDIA_ROOT = BASE_DIR + "/uploads/"
 MEDIA_URL = "/user-media/"
 
 print(f"media_root= {MEDIA_ROOT}")
 print(f"media_url= {MEDIA_URL}")
+print(f"static file dirs= {STATICFILES_DIRS}")
